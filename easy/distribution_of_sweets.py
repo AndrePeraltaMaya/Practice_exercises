e7901e8d19f56eb2ede8bfe37d2afc677b6596c2
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-05 18:48:10 -0500

    Update distribution_of_sweets.py

--- easy/distribution_of_sweets.py
+++ easy/distribution_of_sweets.py
@@ -39,3 +39,8 @@ def distribution(siblings,popsicles):
 		print("give away")
 	else:
 		print("eat them yourself")
+		
+siblings = input("Number of brothers")
+popsicles = input("number of sweets")
+
+distribution(siblings,popsicles)
