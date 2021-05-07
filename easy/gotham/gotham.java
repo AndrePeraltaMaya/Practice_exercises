3680578f5d49807052cfbe369229b5fe6260f01a
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-07 00:19:02 -0500

    Create gotham.java

---
+++ easy/gotham/gotham.java
@@ -0,0 +1,26 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *06/05/2021
+ * @author Andre Peralta
+ */
+public class metodo {
+    public static void main(String[] args) {
+        System.out.println("Number of enemys :");
+        Scanner scan = new Scanner(System.in);
+        
+        int enemys = scan.nextInt();
+        
+	if(enemys < 5 ){
+            System.out.println("I got this!");
+        }
+        else if(enemys >= 5 && enemys < 10){
+            System.out.println("Help me Batman");
+        }
+        else if(enemys > 10){
+            System.out.println("Good Luck out there!");
+        }
+   }
+}
