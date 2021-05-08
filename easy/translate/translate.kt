a475dfc7fea4b33f1f8b1b320e868225f058389e
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-08 17:26:25 -0500

    Create translate.kt

---
+++ easy/translate/translate.kt
@@ -0,0 +1,16 @@
+fun inverted(text:String): String{
+
+    return(text.reversed())    
+}
+
+
+fun main(args: Array<String>){
+	    
+    println("Text : ")
+    var text = "howdy"
+    
+	println("Text inverted : ")
+	println(inverted(text))
+    
+
+}
