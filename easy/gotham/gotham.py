e74b16eea5acec1562f2a1f0e620531b16ab7474
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-08 18:24:06 -0500

    Update gotham.py

--- easy/gotham/gotham.py
+++ easy/gotham/gotham.py
@@ -2,7 +2,7 @@ def fight(enemys):
 
 	if enemys < 5:
 		print("I got this!")
-	if enemys >= 5 and enemys < 10:
+	if enemys >= 5 and enemys <= 10:
 		print("Help me Batman")
 	if enemys > 10:
 		print("Good Luck out there!")
