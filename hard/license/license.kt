a5fcf4eb45dbf1e6e45435a5ac4fe5fb3c3ba2e1
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-09 00:15:42 -0500

    Update license.kt

--- hard/license/license.kt
+++ hard/license/license.kt
@@ -1,4 +1,4 @@
-fun minutes(mi_name:String,names:String,agents:Int):Int{
+fun minutes(mi_name:String,names:String,agents:Int):Double{
     
     //Creamos una lista a partir del string
     var names_list = names.split(" ")
@@ -9,23 +9,26 @@ fun minutes(mi_name:String,names:String,agents:Int):Int{
     //Obtenemos la posición de mi nombre con la lista ya ordenada
     var posicion = names_list.indexOf(mi_name)
     //Hacemos el pequeno calculo con respecto al número de agentes
-    var calculation = (posicion+1/agents)*20;
-    
-    return(calculation)
+    var var1:Double = (posicion+1)/agents.toDouble()//2/5 es 2 a menos que use esto
+    var time = (var1*20)+(var1*20)%20
+
+    return(time)
 }
 
 fun main(args: Array<String>){
 	
     println("Mi name : ")
-    var mi_name = readLine()
+    //var mi_name = readLine()
     
 	println("Names list : ")
-    var names = readLine()
+    //var names = readLine()
     
 	println("Agents : ")
-    var agents = readLine()!!.toInt()	
+    //var agents = readLine()!!.toInt()	
     
-    println(minutes(mi_name,names,agents))    
+    //println(minutes(mi_name,names,agents))    
+    println(minutes("Sara","Adam Caroline Rebecca Frank",2))    
+    
     
 
 }
