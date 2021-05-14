0c8b325f931603c72ae527e7c164c0c340500397
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-14 11:34:55 -0500

    Update military_time.py

--- medium/military_time/military_time.py
+++ medium/military_time/military_time.py
@@ -1,18 +1,26 @@
-def military_time(time):
+fun military_time(time:String):String{
 
-	time_list = time[:-2].split(':')
+	var time_list:List<String>  = time.substring(0,time.length-2).split(':') 
+    var time0 = ""
 
-	if time[-2:] == 'PM' and time_list[0] != '12':
-		time_list[0] = int(time_list[0]) + 12
-	else:
-		if int(time_list[0]) < 10:
-			time_list[0] = '0' + time_list[0]
-		print(time_list[0] + ':' + time_list[1])
-		return None
-
-	print(str(time_list[0]) + ':' + time_list[1])
-
-
-time = input()
-
-military_time(time)
+	if (time.substring(time.length-2,time.length) == "PM" && time_list[0] != "12"){
+		time0 = (time_list[0].toInt() + 12).toString()
+    }
+	else{
+		if( time_list[0].toInt() < 10){
+			time0 = "0" + time_list[0].toString()
+        }
+		println(time0 + ':' + time_list[1])
+		return ""
+    }
+	println(time0.toString() + ':' + time_list[1])
+    return ""
+}
+fun main(args: Array<String>){
+	
+    println("Time : ")
+    var time = readLine()
+    
+    println(military_time(time))     
+      
+}
