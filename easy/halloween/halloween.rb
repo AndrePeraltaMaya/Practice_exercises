a846d2ec02acab4bf1326682321acc1616a2383f
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-20 11:47:32 -0500

    Create halloween.rb

---
+++ easy/halloween/halloween.rb
@@ -0,0 +1,17 @@
+"""
+@author	André Peralta
+@version 20/05/2021
+"""
+
+def dolar_house houses 
+
+	return (2*100/houses.to_f).ceil
+
+end
+
+
+puts "Number of houses: "
+houses = gets.to_i
+
+print dolar_house(houses)
+
