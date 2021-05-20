7f103bbace53b69a117afd8d3c4ad1f00fc48229
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-20 12:47:44 -0500

    Create fruit.rb

---
+++ easy/fruit/fruit.rb
@@ -0,0 +1,19 @@
+"""
+@author	André Peralta
+@version 20/05/2021
+"""
+
+def pies n_fruits 
+
+	apples = n_fruits/2 
+
+	return apples/3
+
+end
+
+
+puts "Number of fruits in the bowl: "
+n_fruits = gets.to_i
+
+print pies(n_fruits)
+
