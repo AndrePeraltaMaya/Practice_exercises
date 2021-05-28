b99459c61928d031e0ba787b3fd13f288a953e7e
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-28 00:36:18 -0500

    Create fruit.cpp

---
+++ easy/fruit/fruit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int fruits;
+
+    cout << "Introduce el número de frutas: ";
+    cin >> fruits;
+
+    int apples = fruits/2;
+
+    int n_pies = apples/3;
+
+    cout << n_pies;
+
+}
