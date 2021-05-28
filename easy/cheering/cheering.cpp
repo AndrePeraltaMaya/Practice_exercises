6c8c03893531df25fa0c6974e6dc504deaa43c31
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-28 00:53:32 -0500

    Create cheering.cpp

---
+++ easy/cheering/cheering.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int yards;
+    
+    cin >> yards;
+    
+    if(yards > 10){
+    
+        cout << "High Five";
+        
+    }else if(yards < 1 ){
+        cout << "shh";
+    }else{
+        while(yards >0){
+            cout << "Ra!";
+            yards--;
+        }    
+    }
+}
