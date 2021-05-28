4713378d372192bcdb17846ef3901646c02b7072
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-28 00:20:55 -0500

    Create halloween.cpp

---
+++ easy/halloween/halloween.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+
+    int houses;
+
+    cout << "Introduce el número de casas: ";
+    cin >> houses;
+
+    int porcent = ceil((2*100)/houses);
+
+    cout << porcent;
+
+
+}
+
