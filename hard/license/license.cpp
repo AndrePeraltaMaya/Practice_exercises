a055364974e37c63dd51bd8509947eb0f59609d7
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-28 13:15:36 -0500

    Create license.cpp

---
+++ hard/license/license.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+int main(){
+
+    std::string name;
+    std::string names[5];
+    int agents;
+
+    std::cout << "Introduce mi nombre : " ;
+    std::cin >>  name;
+
+    std::cout << "Introduce el número de agentes : ";
+    std::cin >> agents;
+
+    for(int i = 1;i< 5;i++){
+        std::cin >> names[i];
+    }
+
+    names[0] = name;
+    sort(begin(names), end(names)); /**Ordenamos el vector**/
+
+    int pos_fila = 1;/**posicion minima en la que podemos encontrarnos**/
+    int time = 20;
+    for (const auto &str : names) {/**Encontrar nuestra posición en la fila**/
+            if(str == name)break;
+            pos_fila++;
+            if (pos_fila%agents == 0)time += 20;
+    }
+    std::cout << time;
+}
