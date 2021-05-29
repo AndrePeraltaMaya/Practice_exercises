ae65e9edbb8c998755321874952a5537c94c6d7d
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-05-28 23:49:48 -0500

    Create secret_message.cpp

---
+++ medium/secret_message/secret_message.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctype.h>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+int main(){
+    string text;
+
+    cout << "Introduce el texto: ";
+    getline(cin, text);
+    string textReverse[text.length()];
+
+    for(const &i : text){
+        if((i >= 65 && i <= 90)){
+           int diferencia = 90-(i-65);
+           printf("%c",diferencia);
+        }
+        if((i >= 97 && i <= 122)){
+             int diferencia = 122-(i-97)+32;
+             printf("%c",diferencia);
+        }
+        if( i == 32){
+            printf("%c",32);
+        }
+    }
+}
