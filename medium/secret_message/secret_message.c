95b0913afb316c043d41be7b2d04f68f352d779f
André Peralta <83569113+AndrePeraltaMaya@users.noreply.github.com>
2021-06-02 13:26:30 -0500

    Create secret_message.c

---
+++ medium/secret_message/secret_message.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char text[100];
+
+    fgets(text,100,stdin);
+    strlwr(text);
+    for(int i = 0 ; i < strlen(text) ; i++){
+
+        if((text[i] >= 65 && text[i] <= 90)){
+           int diferencia = 90-(text[i]-65)+32;
+           printf("%c",diferencia);
+        }
+        else if((text[i] >= 97 && text[i] <= 122)){
+             int diferencia = 122-(text[i]-97);
+             printf("%c",diferencia);
+        }
+        else if( text[i] == 32){
+            printf("%c",32);
+        }
+    }
+    return 0;
+}
